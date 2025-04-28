3f03d3853a0bf4bbc5b3a0f6b3796a34526f11a0
Daniel Alexis Cruz <dcruz@up.edu.ph>
2025-04-29 00:29:18 +0800

    Integrate retention of data in editing entries and fixed minor layout issues in modal

--- budget/forms.py
+++ budget/forms.py
@@ -124,8 +124,7 @@ class CategoryForm(forms.ModelForm):
 
         # apply your existing styling/placeholder
         self.fields['name'].widget.attrs.update({
-            'class': 'mt-1 block w-full border-gray-300 rounded-md shadow-sm '
-                     'focus:ring-indigo-500 focus:border-indigo-500',
+            'class': 'mt-1 block w-full border-gray-300 rounded-md py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500 border',
             'placeholder': 'New category',
         })
 
